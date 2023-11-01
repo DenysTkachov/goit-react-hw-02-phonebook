242e40c2cd19e0f4c7264228e8e40907bfda2f65
DenysTkachov <D.Tkachev@ukr.net>
2023-11-01 16:33:38 +0100

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,48 @@
-export const App = () => {
-  return (
-    <div>
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleNameChange = e => {
+    this.setState({ name: e.target.value });
+  };
+  handleAddContact = () => {
+    if (this.state.name.trim() === '') {
+      return;
+    }
+    const newContact = {
+      id: nanoid(),
+      name: this.state.name,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <h2>Name</h2>
+        <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+          required
+        />
+        <button onClick={this.handleAddContact}>Add Contact</button>
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
